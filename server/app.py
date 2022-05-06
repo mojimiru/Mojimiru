3daeba338ce0492dfa036c550944d3035b8db7f0
mojimiru <mojimiru.dev@gmail.com>
2022-05-07 06:07:13 +0900

    🐛 changed flask app setting

--- server/app.py
+++ server/app.py
@@ -1,11 +1,7 @@
 from flask import Flask
 
-app = Flask(__name__, static_folder='.', static_url_path='')
+app = Flask(__name__)
 
-@app.route('/') #type: ignore
-def index():
-    return "HELLO"
-
-if __name__ == '__main__':
-    app.run()
-    # getGlyphSvg(Path(r'assets\fonts\MRT-まるこいあすα かなのみ.ttf'),"wind")
+@app.route('/')
+def hello_world():
+  return "aaaaaaaa"
